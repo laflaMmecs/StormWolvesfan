619605f9a087cec25e8e40ad5d186caec7982c6b
laflaMme <maks.ponomarenko.2021@mail.ru>
2023-04-08 11:54:58 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          storm wolves
         </p>
         <a
           className="App-link"
